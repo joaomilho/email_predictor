652c7b1bfabf33e76ec4908bcc64ed5de5f036fa
Juan Lulkin <maiz@lulk.in>
2014-06-25 15:54:45 +0300

    Add spec for last initial scenario

--- spec/email_predictor_spec.rb
+++ spec/email_predictor_spec.rb
@@ -17,5 +17,17 @@ describe EmailPredictor do
         expect(prediction).to eq('ludwig.wittgenstein@cambridge.edu')
       end
     end
+
+    context 'given a dataset with first name and last initial' do
+      let(:dataset) do
+        {
+          'Francis Bacon' => 'francis.b@cambridge.edu'
+        }
+      end
+
+      it 'returns an email with first name and last initial' do
+        expect(prediction).to eq('ludwig.w@cambridge.edu')
+      end
+    end
   end
 end
