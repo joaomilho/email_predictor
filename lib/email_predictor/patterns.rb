3825f32cc210e9751ff5b38f15fcdb5549ef7d47
Juan Lulkin <maiz@lulk.in>
2014-07-08 03:19:58 +0300

    Add patterns module, with autoload

---
+++ lib/email_predictor/patterns.rb
@@ -0,0 +1,19 @@
+module EmailPredictor
+  module Patterns
+    autoload :Initials,       'email_predictor/patterns/initials'
+    autoload :InitialName,    'email_predictor/patterns/initial_name'
+    autoload :InitialSurname, 'email_predictor/patterns/initial_surname'
+    autoload :Full,           'email_predictor/patterns/full'
+
+    def self.patterns
+      constants.map{|pattern| self.const_get(pattern) }
+    end
+
+    def self.evaluate user
+      patterns.detect do |pattern|
+        pattern.match?(user)
+      end
+    end
+  end
+end
+
