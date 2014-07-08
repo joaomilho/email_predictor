8af7ea64d0e80e77247e2d84b246cddce238cf4d
Juan Lulkin <maiz@lulk.in>
2014-07-08 03:22:18 +0300

    Finish predictor using dataset

--- lib/email_predictor/predictor.rb
+++ lib/email_predictor/predictor.rb
@@ -1,9 +1,13 @@
-class EmailPredictor::Predictor
-  def initialize(dataset)
-    @dataset = dataset
-  end
+module EmailPredictor
+  class Predictor
+    def initialize(bulk_data)
+      @dataset = Dataset.new(bulk_data.values)
+    end
 
-  def predict(name, domain)
-    'ludwig.wittgenstein@cambridge.edu'
+    def predict(name, domain)
+      @dataset[domain].map do |pattern|
+        pattern.format(name, domain)
+      end
+    end
   end
 end
